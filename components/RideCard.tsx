ef358eba1bceaa46c0c6a7bf6a7d2bec71000921
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2025-08-21 12:30:55 +0100

    correction to env

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,6 +1,6 @@
 import { Ride } from '@/types/types';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 const RideCard = ({
   ride: {
@@ -21,7 +21,17 @@ const RideCard = ({
   ride: Ride;
 }) => {
   return (
-    <View>
+    <View className='flex flex-row items-center justify-center p-4 mb-3 bg-white rounded-lg shadow-sm shadow-neutral-300'>
+      <View className='flex flex-row items-center justify-between p-3'>
+        <View className='flex flex-row items-center justify-center'>
+          <Image
+            source={{
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude}, ${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+            }}
+            className='w-[80px] h-[90px] rounded-lg'
+          />
+        </View>
+      </View>
       <Text>hello</Text>
     </View>
   );
